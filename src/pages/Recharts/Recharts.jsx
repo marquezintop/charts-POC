315a5eb554d3c2c533af1a969a45d6f438a6fd8d
marquezintop <marquez.cmbh@gmail.com>
2023-11-09 15:34:57 -0300

    initial commit

--- src/pages/Recharts/Recharts.jsx
+++ src/pages/Recharts/Recharts.jsx
@@ -1,9 +1,16 @@
+import { Pie, PieChart } from "recharts";
 import { Container } from "./style";
 
 export default function Recharts() {
+    const data = [{value: 10, label: 'oi'}, {value: 100, label: 'olá'}]
+
     return (
         <Container>
-            Recharts
+            <PieChart>
+                <Pie dataKey={data}>
+
+                </Pie>
+            </PieChart>
         </Container>
     )
 }
